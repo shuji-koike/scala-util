244371ba32069b395edb287c15cd471321d671d6
Shuji Koike <shuji.koike@me.com>
2017-03-13 23:54:29 +0900

    add package object util

---
+++ src/main/scala/org/koike/shuji/util/package.scala
@@ -0,0 +1,17 @@
+package org.koike.shuji
+
+package object util {
+  implicit class PrettyPrint[A](a: A) {
+    def pp: A = {
+      Logger.pp.info(a.toString)
+      a
+    }
+  }
+
+  implicit class Tap[A](a: A) {
+    def tap(fn: A => Unit): A = {
+      fn(a)
+      a
+    }
+  }
+}
